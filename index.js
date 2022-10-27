98d1c9ca25e269862766fde0f5331061c902c1b1
uddinmayraj <mayrajuddin03@gmail.com>
2022-10-27 22:01:39 +0600

    added id

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 app.use(cors())
 const catagories = require('./data/catagory.json')
-const languages = require('./data/allLanguage.json')
+const languages = require('./data/languagedata.json')
 app.get('/', (req, res) => {
     res.send('server runing')
 })
 app.get('/language-catagory', (req, res) => {
     res.send(catagories)
 })
-app.get('/allLanguage', (req, res) => {
+app.get('/languagedata', (req, res) => {
     res.send(languages)
+})
+app.get('/languagedata/:id', (req, res) => {
+    const id = req.params.id;
+    const language = languages.find(lan => lan.id == id)
+    res.send(language)
 })
 app.listen(port, () => {
     console.log('language server running');
